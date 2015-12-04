7b302e3cf28c4517cc0fba7131ae7a58a79de8f5
Rajib Singh <rajibsingh@gmail.com>
2015-12-04 01:09:19 -0500

    diagonal-difference works now

--- diagonal-difference.swift
+++ diagonal-difference.swift
@@ -2,12 +2,12 @@
 
 // number of elements
 var n = Int(readLine()!)!
-var matrix = Array<Array<String>>()
+var matrix = Array<Array<Int>>()
 
 
 // read array
 for (var i = 0; i < n; i++) {
-	var arr = readLine()!.characters.split(" ").map(String.init)
+	var arr = readLine()!.characters.split(" ").map( { Int(String($0)) ?? 0 } )
 	matrix.append(arr);
 }
 
@@ -15,6 +15,18 @@ for (var i = 0; i < n; i++) {
 for (var i = 0; i < n; i++) {
 	print(matrix[i])
 }
+
+//calulate tl to br
+var tlbr = 0
+var trbl = 0
+
+for (var i = 0; i < n; i++) {
+	tlbr += matrix[i][i]
+	trbl += matrix[i][n-1-i]
+}
+print(tlbr)
+print(trbl)
+print(abs(tlbr - trbl))
 
 // variable to hold the sum of the array elements
 //var sum = 0
