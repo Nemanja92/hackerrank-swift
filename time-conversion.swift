388542afe2684a7f0350c1d19204856d90fd3ff8
Rajib Singh <rajibsingh@gmail.com>
2015-12-06 02:55:18 -0500

    don't want to admit how long time-conversion ended up taking me

--- time-conversion.swift
+++ time-conversion.swift
@@ -1,14 +1,27 @@
-//staircase
+//time conversion
 
 // number of elements
-let n = Int(readLine()!)!
+//var input = "07:05:45PM"
+//var input = "12:40:22AM"
+var input = "12:45:54PM"
 
-var hash = "#"
-for (var i = 0; i < n; i++) {
-	let spaces = String(count: n - i - 1, repeatedValue: Character(" "))	
-	print(spaces + hash)	
-	hash += "#"
+//var input = readLine()!
+let amPmSliceEnd = input.characters.endIndex
+let amPmSliceStart = amPmSliceEnd.advancedBy(-2)
+let amPm = input[amPmSliceStart..<amPmSliceEnd]
+var time = input[input.startIndex..<amPmSliceStart]
+var hrsMinSecArray = time.characters.split{$0 == ":"}.map(String.init)
+var hr = hrsMinSecArray[hrsMinSecArray.startIndex]
+var hrInt: Int = Int(hr)!
+
+if amPm == "PM" && hrInt != 12 {
+    hrInt += 12
+    hrsMinSecArray[0] = String(hrInt)
+    time = hrsMinSecArray[0] + ":" + hrsMinSecArray[1] + ":" + hrsMinSecArray[2]
+} else if amPm == "AM" && hrInt == 12 {
+    hrsMinSecArray[0] = "00"
+    time = hrsMinSecArray[0] + ":" + hrsMinSecArray[1] + ":" + hrsMinSecArray[2]
 }
 
 
-
+print(time)
