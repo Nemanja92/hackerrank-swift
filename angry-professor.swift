a7a094fdabe76e5bca7c17ccb9d112217a563cde
Rajib Singh <rajibsingh@gmail.com>
2015-12-08 00:02:41 -0500

    finally working

--- angry-professor.swift
+++ angry-professor.swift
@@ -1,8 +1,31 @@
 // angry professor
 
 // number of elements
-let classes = readLine()!
-let classesInt = Int(classes)
+var input = readLine()!
+let classes = Int(input)
+var output: [String] = []
 
-for 
+for var cls = 0; cls < classes; cls++ {
+    var input2 = readLine()!.characters.split(" ").map( { Int(String($0)) ?? 0 } )
+    var n = input2[0]
+    var k = input2[1]
+    input2 = readLine()!.characters.split(" ").map( { Int(String($0)) ?? 0 } )
+    var count = 0
+    for var i = 0; i < n; i++ {
+        if input2[i] <= 0 {
+            count++
+        }
+    }
+    if count < k {
+        output.append("YES")
+    }
+    else {
+        output.append("NO")
+    }
+}
+
+for str in output {
+    print(str)
+}
+
 
