46ac71c86dd8a737041c7f5f62b601a0524fcf56
Rajib Singh <rajibsingh@yahoo.com>
2016-04-28 17:23:45 -0400

    added new files

---
+++ lonely_integer.swift
@@ -0,0 +1,42 @@
+// lonely_integer
+
+// number of elements
+//var input = readLine()!
+var input = 3
+
+// var arry = [Int]()
+// for index in 0...arraySize {
+	// arry.append(index)
+// }
+
+let arry = [0, 1, 2]
+
+for index in 0...arry.count-1 {
+	print(arry[index])
+}
+// // var output: [String] = []
+
+// for var cls = 0; cls < classes; cls++ {
+//     var input2 = readLine()!.characters.split(" ").map( { Int(String($0)) ?? 0 } )
+//     var n = input2[0]
+//     var k = input2[1]
+//     input2 = readLine()!.characters.split(" ").map( { Int(String($0)) ?? 0 } )
+//     var count = 0
+//     for var i = 0; i < n; i++ {
+//         if input2[i] <= 0 {
+//             count++
+//         }
+//     }
+//     if count < k {
+//         output.append("YES")
+//     }
+//     else {
+//         output.append("NO")
+//     }
+// }
+
+// for str in output {
+//     print(str)
+// }
+
+
